0bb254e6a478f4a12b1cc9d0546f9bf2208d7f3d
hnquy156 <quyhn@mioapp.vn>
2023-03-13 20:09:55 +0700

    Add body of folder page

--- src/components/pages/user/Folder.js
+++ src/components/pages/user/Folder.js
@@ -1,10 +1,13 @@
 import { Box } from '@mui/material';
 import FolderHeader from '../../folder-header/FolderHeader';
+import StudySets from '../../study-sets/StudySets';
 
 const Folder = () => {
   return (
     <Box m={10} mt={4}>
       <FolderHeader />
+      <Box mb={14} />
+      <StudySets title="Sets in Folder" />
     </Box>
   );
 };
